0e81c864914e0d614cfcdd13a2243fc99acc8c23
Raunak <raunak51299@gmail.com>
2023-10-18 14:22:38 +0530

    bug fix

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //import cron
 import cron from 'node-cron';
+import fs from 'fs';  
 import {fetchRedditPost, generateCommentsString} from './reddit_scraper.js';
 import './telegram_bot.js';
-// Run everyday at 9:00 A.M IST
+// Run everyday at 9:00 AM IST every day
 cron.schedule('0 9 * * *', async () => {
     console.log("Running cron job...");
     const redditPostComments = await fetchRedditPost();
