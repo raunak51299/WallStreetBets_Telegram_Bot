d4ecdfcb86f6f8afa365591d772a0dc754958762
Raunak <raunak51299@gmail.com>
2023-10-19 10:50:14 +0530

    time fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ import cron from 'node-cron';
 import fs from 'fs';  
 import {recommendStocks} from './openai.js';
 import './telegram_bot.js';
-// Run everyday at 9:00 AM IST every day
-cron.schedule('0 9 * * *', async () => {
+// Run everyday at 5:30 AM IST every day
+cron.schedule('30 5 * * *', async () => {
     console.log("Running cron job...");
     const text = await recommendStocks();
     fs.writeFileSync('output.txt', text);
