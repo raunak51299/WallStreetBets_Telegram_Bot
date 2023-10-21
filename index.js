260c1c2cf15308f5e2cd51871ca4461978a762de
Raunak <raunak51299@gmail.com>
2023-10-22 01:53:52 +0530

    added date and time

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-//import cron
 import cron from 'node-cron';
 import fs from 'fs';  
 import {recommendStocks} from './openai.js';
 import './telegram_bot.js';
+    
 // Run everyday at 7:30 AM IST every day
 cron.schedule('30 7 * * *', async () => {
-    console.log("Running cron job...");
+    const date = new Date();
+    console.log("Running cron job... at " + date.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }));
     const text = await recommendStocks();
     fs.writeFileSync('output.txt', text);
     console.log("Cron job finished.");
